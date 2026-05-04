720c8b57f4797106bca5d52e53e7007f5e2bc182
Shubhdeep Sarkar <sarkarshubhdeep1@gmail.com>
2026-05-04 15:16:33 -0400

    Initial Commit

---
+++ Counts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Counts
+//
+//  Created by Shubhdeep Sarkar on 5/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Counts/CountsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountsApp.swift
+//  Counts
+//
+//  Created by Shubhdeep Sarkar on 5/4/26.
+//
+
+import SwiftUI
+
+@main
+struct CountsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
